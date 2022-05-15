e46a747c224876df1efab48d2871fb00809e2378
Finn <contact@bechinka.eu>
2022-05-15 23:55:44 +0200

    files

---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <vector>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include "GLSLProgram.h"
+#include "GLTools.h"
+#include "sphere.h"
+
+
+
+float distance = 4.0f;
+Sphere sphere(1, 1);
+System coordinates(sphere.radius);
+
+
+bool init()
+{
+    glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+    glEnable(GL_DEPTH_TEST);
+    view = glm::lookAt(glm::vec3(0.5f, 1.5f, distance), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    if (!program.compileShaderFromFile("shader/simple.vert", cg::GLSLShader::VERTEX)) {
+        std::cerr << program.log();
+        return false;
+    }
+
+    if (!program.compileShaderFromFile("shader/simple.frag", cg::GLSLShader::FRAGMENT)) {
+        std::cerr << program.log();
+        return false;
+    }
+
+    if (!program.link()) {
+        std::cerr << program.log();
+        return false;
+    }
+
+    sphere.init_sphere();
+    coordinates.init_system();
+
+    return true;
+}
+
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    sphere.render();
+    coordinates.render();
+}
+
+
+void glutDisplay()
+{
+    render();
+    glutSwapBuffers();
+}
+
+
+void glutResize(int width, int height)
+{
+    height = height < 1 ? 1 : height;
+    glViewport(0, 0, width, height);
+    projection = glm::perspective(45.0f, (float)width / height, 0.1f, 100.0f);
+}
+
+
+void glutKeyboard(unsigned char keycode, int x, int y)
+{
+    switch (keycode) {
+    case 27: glutDestroyWindow(glutGetWindow()); return;
+    case 'r':
+        sphere = Sphere(sphere.radius + 0.3, sphere.n);
+        init();
+        break;
+    case 'R':
+        sphere = Sphere(sphere.radius - 0.3, sphere.n);
+        init();
+        break;
+    case 'a':
+        distance = distance + 0.3f;
+        view = glm::lookAt(glm::vec3(0.5f, 1.5f, distance), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        break;
+    case 's':
+        distance = distance - 0.3f;
+        view = glm::lookAt(glm::vec3(0.5f, 1.5f, distance), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        break;
+    case 'k':
+        if (coordinates.radius == sphere.radius) { coordinates = System(100.0f); }
+        else { coordinates = System(sphere.radius); }
+        coordinates.init_system();
+        break;
+    case 'x':
+        sphere.rotateX(0.05f);
+        break;
+    case 'y':
+        sphere.rotateY(0.05f);
+        break;
+    case 'z':
+        sphere.rotateZ(0.05f);
+        break;
+    case 'n':
+        sphere.resetRotation();
+        break;
+    case '+':
+        if (sphere.n < 4) {
+            sphere = Sphere(sphere.radius, sphere.n + 1);
+            init();
+        }
+        break;
+    case '-':
+        if (sphere.n > 0) {
+            sphere = Sphere(sphere.radius, sphere.n - 1);
+            init();
+        }
+        break;
+    }
+
+    glutPostRedisplay();
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(40, 40);
+    glutInit(&argc, argv);
+    glutInitContextVersion(4, 3);
+    glutInitContextFlags(GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
+    glutCreateWindow("Aufgabenblatt 02");
+    if (glewInit() != GLEW_OK) { return -1; }
+    glutReshapeFunc(glutResize);
+    glutDisplayFunc(glutDisplay);
+    glutKeyboardFunc(glutKeyboard);
+
+    bool result = init();
+    if (!result) { return -2; }
+    glutMainLoop();
+
+    return 0;
+}
---
+++ sphere.h
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include "GLSLProgram.h"
+#include "GLTools.h"
+
+
+cg::GLSLProgram program;
+glm::mat4x4 view;
+glm::mat4x4 projection;
+
+
+class Object {
+public:
+    inline Object() :
+        vao(0),
+        positionBuffer(0),
+        colorBuffer(0),
+        indexBuffer(0)
+    {}
+
+    inline ~Object() { // GL context must exist on destruction
+        glDeleteVertexArrays(1, &vao);
+        glDeleteBuffers(1, &indexBuffer);
+        glDeleteBuffers(1, &colorBuffer);
+        glDeleteBuffers(1, &positionBuffer);
+    }
+
+    void init(std::vector<glm::vec3> vertices, std::vector<glm::vec3> colors, std::vector<GLushort> indices)
+    {
+        GLuint programId = program.getHandle();
+        GLuint pos;
+
+        glGenVertexArrays(1, &this->vao);
+        glBindVertexArray(this->vao);
+
+        glGenBuffers(1, &this->positionBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, this->positionBuffer);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
+
+        pos = glGetAttribLocation(programId, "position");
+        glEnableVertexAttribArray(pos);
+        glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glGenBuffers(1, &this->colorBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, this->colorBuffer);
+        glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), colors.data(), GL_STATIC_DRAW);
+
+        pos = glGetAttribLocation(programId, "color");
+        glEnableVertexAttribArray(pos);
+        glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glGenBuffers(1, &this->indexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->indexBuffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
+
+        glBindVertexArray(0);
+        this->model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+    }
+
+    void render()
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+        program.use();
+        program.setUniform("mvp", projection * view * this->model);
+
+        glBindVertexArray(this->vao);
+        glDrawElements(GL_TRIANGLES, 2000, GL_UNSIGNED_SHORT, 0);
+        glBindVertexArray(0);
+    }
+
+    GLuint vao;
+    GLuint positionBuffer;
+    GLuint colorBuffer;
+    GLuint indexBuffer;
+    glm::mat4x4 model;
+};
+
+
+class Sphere : public Object {
+public:
+
+    float radius, rotX, rotY, rotZ;
+    int n;
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec3> colors;
+    std::vector<GLushort> indices;
+
+    inline Sphere(float radius, int n) :
+        radius(radius), n(n), rotX(0), rotY(0), rotZ(0)
+    {}
+
+    void init_sphere()
+    {
+        glm::vec3 top = { 0, radius, 0 };
+
+        glm::vec3 a = { radius, 0, radius };
+        glm::vec3 b = { radius, 0, -radius };
+        glm::vec3 c = { -radius , 0, -radius };
+        glm::vec3 d = { -radius , 0, radius };
+
+        // topside 
+        triangle(top, ((a - top) / glm::vec3{ n + 1, n + 1, n + 1 }), (b - top) / (glm::vec3{ n + 1, n + 1, n + 1 }));
+        triangle(top, ((b - top) / glm::vec3{ n + 1, n + 1, n + 1 }), (c - top) / (glm::vec3{ n + 1, n + 1, n + 1 }));
+        triangle(top, ((c - top) / glm::vec3{ n + 1, n + 1, n + 1 }), (d - top) / (glm::vec3{ n + 1, n + 1, n + 1 }));
+        triangle(top, ((d - top) / glm::vec3{ n + 1, n + 1, n + 1 }), (a - top) / (glm::vec3{ n + 1, n + 1, n + 1 }));
+
+        int vsz = vertices.size();
+        for (int i = 0; i < vsz; i++)
+        {
+            vertices.push_back(-vertices[i]);
+
+        }
+
+        int isz = indices.size();
+        for (int i = 0; i < isz; i++)
+        {
+            indices.push_back(indices[i] + isz);
+        }
+
+        for (int x = 0; x < vertices.size(); x++) {
+            glm::vec3 rv = ((vertices[x] - glm::vec3{ 0, 0, 0 }));
+            float dist = sqrt((rv.x * rv.x) + (rv.y * rv.y) + (rv.z * rv.z));
+            float scale = (radius) / dist;
+            vertices[x].x *= scale;
+            vertices[x].y *= scale;
+            vertices[x].z *= scale;
+            colors.push_back({ 1.0, 1.0, 0.0 });
+        }
+
+        init(vertices, colors, indices);
+    }
+
+    void rotateX(float rotation) {
+        rotX += rotation;
+        for (int i = 0; i < vertices.size(); i++) {
+            float y = (vertices[i][1] * cos(rotation)) - (vertices[i][2] * sin(rotation));
+            float z = (vertices[i][1] * sin(rotation)) + (vertices[i][2] * cos(rotation));
+            vertices[i] = glm::vec3(vertices[i][0], y, z);
+        }
+        init(vertices, colors, indices);
+    }
+
+    void rotateY(float rotation) {
+        rotY += rotation;
+        for (int i = 0; i < vertices.size(); i++) {
+            float x = (vertices[i][0] * cos(rotation)) + (vertices[i][2] * sin(rotation));
+            float z = (-vertices[i][0] * sin(rotation)) + (vertices[i][2] * cos(rotation));
+            vertices[i] = glm::vec3(x, vertices[i][1], z);
+        }
+        init(vertices, colors, indices);
+    }
+
+    void rotateZ(float rotation) {
+        rotZ += rotation;
+        for (int i = 0; i < vertices.size(); i++) {
+            float x = (vertices[i][0] * cos(rotation)) - (vertices[i][1] * sin(rotation));
+            float y = (vertices[i][0] * sin(rotation)) + (vertices[i][1] * cos(rotation));
+            vertices[i] = glm::vec3(x, y, vertices[i][2]);
+        }
+        init(vertices, colors, indices);
+    }
+
+    void resetRotation() {
+        rotateX(-rotX); rotX = 0;
+        rotateY(-rotY); rotY = 0;
+        rotateZ(-rotZ); rotZ = 0;
+        init(vertices, colors, indices);
+    }
+
+    void triangle(glm::vec3 start, glm::vec3 leftvec, glm::vec3 rightvec)
+    {
+        if (start.y <= 0)
+            return;
+
+        vertices.push_back(start);
+        vertices.push_back(start + leftvec);
+        vertices.push_back(start + rightvec);
+
+        indices.push_back(vertices.size() - 3);
+        indices.push_back(vertices.size() - 2);
+        indices.push_back(vertices.size() - 1);
+
+        triangle(start + leftvec, leftvec, rightvec);
+        triangle(start + rightvec, leftvec, rightvec);
+    }
+
+};
+
+class System : public Object {
+public:
+
+    float radius;
+
+    inline System(float radius) :
+        radius(radius)
+    {}
+
+    void init_system() {
+
+        std::vector<glm::vec3> vertices;
+        std::vector<glm::vec3> colors;
+        std::vector<GLushort> indices;
+
+        vertices.push_back({ 0, 0, 0 });
+        vertices.push_back({ radius, 0, 0 });
+        vertices.push_back({ -radius, 0, 0 });
+        vertices.push_back({ 0, 0, 0 });
+        vertices.push_back({ 0, radius, 0 });
+        vertices.push_back({ 0,-radius, 0 });
+        vertices.push_back({ 0, 0, 0 });
+        vertices.push_back({ 0, 0, radius });
+        vertices.push_back({ 0, 0,-radius });
+
+        colors.push_back({ 1.0, 0.0, 0.0 });
+        colors.push_back({ 1.0, 0.0, 0.0 });
+        colors.push_back({ 1.0, 0.0, 0.0 });
+        colors.push_back({ 0.0, 1.0, 0.0 });
+        colors.push_back({ 0.0, 1.0, 0.0 });
+        colors.push_back({ 0.0, 1.0, 0.0 });
+        colors.push_back({ 0.0, 0.0, 1.0 });
+        colors.push_back({ 0.0, 0.0, 1.0 });
+        colors.push_back({ 0.0, 0.0, 1.0 });
+
+        indices = {
+            2, 0, 1,
+            5, 3, 4,
+            8, 6, 7
+        };
+
+        init(vertices, colors, indices);
+    }
+
+};
