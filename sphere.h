2a6dba2bb78d1e7304f7a4d652d20b08314c3cb9
Finn <26581865+shakerrrr@users.noreply.github.com>
2022-06-09 04:21:44 +0200

    proper rotation with degrees

--- sphere.h
+++ sphere.h
@@ -17,12 +17,39 @@ glm::mat4x4 projection;
 
 class Object {
 public:
+	GLuint vao;
+	GLuint positionBuffer;
+	GLuint colorBuffer;
+	GLuint indexBuffer;
+	glm::mat4x4 model;
+	glm::vec3 coords;
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> colors;
+	std::vector<GLushort> indices;
+	double rot_x, rot_y, rot_z;
+
 	inline Object(glm::vec3 coords) :
 		vao(0),
 		positionBuffer(0),
 		colorBuffer(0),
 		indexBuffer(0),
-		coords(coords)
+		coords(coords),
+		rot_x(0),
+		rot_y(0),
+		rot_z(0)
+	{
+		this->model = glm::translate(glm::mat4(1.0f), coords);
+	}
+
+	inline Object(glm::vec3 coords, double rx, double ry, double rz) :
+		vao(0),
+		positionBuffer(0),
+		colorBuffer(0),
+		indexBuffer(0),
+		coords(coords),
+		rot_x(rx),
+		rot_y(ry),
+		rot_z(rz)
 	{
 		this->model = glm::translate(glm::mat4(1.0f), coords);
 	}
@@ -77,27 +104,83 @@ public:
 		glBindVertexArray(0);
 	}
 
-	GLuint vao;
-	GLuint positionBuffer;
-	GLuint colorBuffer;
-	GLuint indexBuffer;
-	glm::mat4x4 model;
-	glm::vec3 coords;
+	void rotateX(double deg) {
+		rot_x += deg;
+		// convert degrees to radians
+		double rad = deg * (glm::pi<double>() / 180);
+		for (int i = 0; i < vertices.size(); i++) {
+			double y = (vertices[i][1] * cos(rad)) - (vertices[i][2] * sin(rad));
+			double z = (vertices[i][1] * sin(rad)) + (vertices[i][2] * cos(rad));
+			vertices[i] = glm::vec3(vertices[i][0], y, z);
+		}
+		init(vertices, colors, indices);
+	}
+
+	void rotateY(double deg) {
+		rot_y += deg;
+		// convert degrees to radians
+		double rad = deg * (glm::pi<double>() / 180);
+		for (int i = 0; i < vertices.size(); i++) {
+			double x = (vertices[i][0] * cos(rad)) + (vertices[i][2] * sin(rad));
+			double z = (-vertices[i][0] * sin(rad)) + (vertices[i][2] * cos(rad));
+			vertices[i] = glm::vec3(x, vertices[i][1], z);
+		}
+		init(vertices, colors, indices);
+	}
+
+	void rotateZ(double deg) {
+		rot_z += deg;
+		// convert degrees to radians
+		double rad = deg * (glm::pi<double>() / 180);
+		for (int i = 0; i < vertices.size(); i++) {
+			double x = (vertices[i][0] * cos(rad)) - (vertices[i][1] * sin(rad));
+			double y = (vertices[i][0] * sin(rad)) + (vertices[i][1] * cos(rad));
+			vertices[i] = glm::vec3(x, y, vertices[i][2]);
+		}
+		init(vertices, colors, indices);
+	}
+
+	void global_rotate_x(double deg) {
+		// convert degrees to radians
+		double rad = deg * (glm::pi<double>() / 180);
+		double y = (coords.y * cos(rad)) - (coords.z * sin(rad));
+		double z = (coords.y * sin(rad)) + (coords.z * cos(rad));
+		coords.y = y;
+		coords.z = z;
+		this->model = glm::translate(glm::mat4(1.0f), coords);
+	}
+
+	void global_rotate_y(double deg) {
+		// convert degrees to radians
+		double rad = deg * (glm::pi<double>() / 180);
+		double x = (coords.x * cos(rad)) + (coords.z * sin(rad));
+		double z = (-coords.x * sin(rad)) + (coords.z * cos(rad));
+		coords.x = x;
+		coords.z = z;
+		this->model = glm::translate(glm::mat4(1.0f), coords);
+	}
+
+	void global_rotate_z(double deg) {
+		// convert degrees to radians
+		double rad = deg * (glm::pi<double>() / 180);
+		double x = (coords.x * cos(rad)) - (coords.y * sin(rad));
+		double y = (coords.x * sin(rad)) + (coords.y * cos(rad));
+		coords.x = x;
+		coords.y = y;
+		this->model = glm::translate(glm::mat4(1.0f), coords);
+	}
 };
 
 class System : public Object {
 public:
 
-	float radius, rotX, rotY, rotZ;
+	double radius;
 
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec3> colors;
-	std::vector<GLushort> indices;
 
-	inline System(float radius, glm::vec3 coords) : Object(coords), radius(radius), rotX(0), rotY(0), rotZ(0)
+	inline System(double radius, glm::vec3 coords) : Object(coords), radius(radius)
 	{}
 
-	inline System(float radius, glm::vec3 coords, float rx, float ry, float rz) : Object(coords), radius(radius), rotX(rx), rotY(ry), rotZ(rz)
+	inline System(double radius, glm::vec3 coords, double rx, double ry, double rz) : Object(coords,rx,ry,rz), radius(radius)
 	{}
 
 	void init_system() {
@@ -128,61 +211,20 @@ public:
 			8, 6, 7
 		};
 
-		rotateX(rotX);
-		rotX = rotX / 2;
-		rotateY(rotY);
-		rotY = rotY / 2;
-		rotateZ(rotZ);
-		rotZ = rotZ / 2;
-
 		init(vertices, colors, indices);
 	}
-
-	void rotateX(float rotation) {
-		rotX += rotation;
-		for (int i = 0; i < vertices.size(); i++) {
-			float y = (vertices[i][1] * cos(rotation)) - (vertices[i][2] * sin(rotation));
-			float z = (vertices[i][1] * sin(rotation)) + (vertices[i][2] * cos(rotation));
-			vertices[i] = glm::vec3(vertices[i][0], y, z);
-		}
-		init(vertices, colors, indices);
-	}
-
-	void rotateY(float rotation) {
-		rotY += rotation;
-		for (int i = 0; i < vertices.size(); i++) {
-			float x = (vertices[i][0] * cos(rotation)) + (vertices[i][2] * sin(rotation));
-			float z = (-vertices[i][0] * sin(rotation)) + (vertices[i][2] * cos(rotation));
-			vertices[i] = glm::vec3(x, vertices[i][1], z);
-		}
-		init(vertices, colors, indices);
-	}
-
-	void rotateZ(float rotation) {
-		rotZ += rotation;
-		for (int i = 0; i < vertices.size(); i++) {
-			float x = (vertices[i][0] * cos(rotation)) - (vertices[i][1] * sin(rotation));
-			float y = (vertices[i][0] * sin(rotation)) + (vertices[i][1] * cos(rotation));
-			vertices[i] = glm::vec3(x, y, vertices[i][2]);
-		}
-		init(vertices, colors, indices);
-	}
-
 };
 
 class Sphere : public Object {
 public:
 
-	float radius, rotX, rotY, rotZ;
+	double radius;
 	int n;
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec3> colors;
-	std::vector<GLushort> indices;
 	System local;
 
-	inline Sphere(float radius, int n, glm::vec3 coords) : Object(coords), radius(radius), n(n), rotX(0), rotY(0), rotZ(0), local(System(1, coords))
+	inline Sphere(double radius, int n, glm::vec3 coords) : Object(coords), radius(radius), n(n), local(System(1, coords))
 	{}
-	inline Sphere(float radius, int n, glm::vec3 coords, float rx, float ry, float rz) : Object(coords), radius(radius), n(n), rotX(rx), rotY(ry), rotZ(rz), local(System(1, coords))
+	inline Sphere(double radius, int n, glm::vec3 coords, double rx, double ry, double rz) : Object(coords,rx, ry, rz), radius(radius), n(n), local(System(1, coords))
 	{}
 
 	void init_sphere()
@@ -217,84 +259,16 @@ public:
 
 		for (int x = 0; x < vertices.size(); x++) {
 			glm::vec3 rv = ((vertices[x] - glm::vec3{ 0, 0, 0 }));
-			float dist = sqrt((rv.x * rv.x) + (rv.y * rv.y) + (rv.z * rv.z));
-			float scale = (radius) / dist;
+			double dist = sqrt((rv.x * rv.x) + (rv.y * rv.y) + (rv.z * rv.z));
+			float scale = radius / dist;
 			vertices[x].x *= scale;
 			vertices[x].y *= scale;
 			vertices[x].z *= scale;
 			colors.push_back({ 1.0, 1.0, 0.0 });
 		}
-		rotateX(rotX);
-		rotX = rotX / 2;
-		rotateY(rotY);
-		rotY = rotY / 2;
-		rotateZ(rotZ);
-		rotZ = rotZ / 2;
 
-		init(vertices, colors, indices);
-	}
+		
 
-	void rotateX(float rotation) {
-		rotX += rotation;
-		for (int i = 0; i < vertices.size(); i++) {
-			float y = (vertices[i][1] * cos(rotation)) - (vertices[i][2] * sin(rotation));
-			float z = (vertices[i][1] * sin(rotation)) + (vertices[i][2] * cos(rotation));
-			vertices[i] = glm::vec3(vertices[i][0], y, z);
-		}
-		init(vertices, colors, indices);
-	}
-
-	void rotateY(float rotation) {
-		rotY += rotation;
-		for (int i = 0; i < vertices.size(); i++) {
-			float x = (vertices[i][0] * cos(rotation)) + (vertices[i][2] * sin(rotation));
-			float z = (-vertices[i][0] * sin(rotation)) + (vertices[i][2] * cos(rotation));
-			vertices[i] = glm::vec3(x, vertices[i][1], z);
-		}
-		init(vertices, colors, indices);
-	}
-
-	void rotateZ(float rotation) {
-		rotZ += rotation;
-		for (int i = 0; i < vertices.size(); i++) {
-			float x = (vertices[i][0] * cos(rotation)) - (vertices[i][1] * sin(rotation));
-			float y = (vertices[i][0] * sin(rotation)) + (vertices[i][1] * cos(rotation));
-			vertices[i] = glm::vec3(x, y, vertices[i][2]);
-		}
-		init(vertices, colors, indices);
-	}
-
-	void global_rotate_x() {
-		float y = (coords.y * cos(0.05f)) - (coords.z * sin(0.05f));
-		float z = (coords.y * sin(0.05f)) + (coords.z * cos(0.05f));
-		coords.y = y;
-		coords.z = z;
-		this->model = glm::translate(glm::mat4(1.0f), coords);
-		local.model = glm::translate(glm::mat4(1.0f), coords);
-	}
-
-	void global_rotate_y() {
-		float x = (coords.x * cos(0.05f)) + (coords.z * sin(0.05f));
-		float z = (-coords.x * sin(0.05f)) + (coords.z * cos(0.05f));
-		coords.x = x;
-		coords.z = z;
-		this->model = glm::translate(glm::mat4(1.0f), coords);
-		local.model = glm::translate(glm::mat4(1.0f), coords);
-	}
-
-	void global_rotate_z() {
-		float x = (coords.x * cos(0.05f)) - (coords.y * sin(0.05f));
-		float y = (coords.x * sin(0.05f)) + (coords.y * cos(0.05f));
-		coords.x = x;
-		coords.y = y;
-		this->model = glm::translate(glm::mat4(1.0f), coords);
-		local.model = glm::translate(glm::mat4(1.0f), coords);
-	}
-
-	void resetRotation() {
-		rotateX(-rotX); rotX = 0;
-		rotateY(-rotY); rotY = 0;
-		rotateZ(-rotZ); rotZ = 0;
 		init(vertices, colors, indices);
 	}
 
@@ -307,9 +281,9 @@ public:
 		vertices.push_back(start + leftvec);
 		vertices.push_back(start + rightvec);
 
-		indices.push_back(vertices.size() - 3);
-		indices.push_back(vertices.size() - 2);
-		indices.push_back(vertices.size() - 1);
+		indices.push_back((GLushort)vertices.size() - 3);
+		indices.push_back((GLushort)vertices.size() - 2);
+		indices.push_back((GLushort)vertices.size() - 1);
 
 		triangle(start + leftvec, leftvec, rightvec);
 		triangle(start + rightvec, leftvec, rightvec);
